936901c829187458c6c10a30ac41e2d02491e1e3
Hiroshi MORISHIGE <hiroshi.morishige@gmail.com>
2021-07-08 12:13:43 +0900

    add: swagger base settings

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  const options = new DocumentBuilder()
+    .setTitle('Web App ToDo')
+    .setDescription('The API documentation for the ToDo app')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors();
+  await app.listen(3000);
 }
 bootstrap();
