6805ba1b37782e88971fab49123903655ce679ad
Hiroshi MORISHIGE <hiroshi.morishige@gmail.com>
2021-07-10 14:33:39 +0900

    upate: emotion setting

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import { Global, css } from '@emotion/react';
+import emotionReset from 'emotion-reset';
 import * as serviceWorker from './serviceWorker';
+import { globalStyle } from './styles/globalStyle';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <Global
+        styles={css`
+          ${emotionReset}
+          ${globalStyle}
+        `}
+      />
       <App />
     </Provider>
   </React.StrictMode>,
