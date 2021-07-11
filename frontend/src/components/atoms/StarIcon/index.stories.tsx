3feffc980979e8f23fdf2af31691f46609f3786d
Hiroshi MORISHIGE <hiroshi.morishige@gmail.com>
2021-07-11 20:09:03 +0900

    :sparkles: add StarIcon components

---
+++ frontend/src/components/atoms/StarIcon/index.stories.tsx
@@ -0,0 +1,16 @@
+/** @jsxImportSource @emotion/react */
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import StarIcon from '.';
+
+export default {
+  title: 'WebToDoApp/Atoms/StarIcon',
+  component: StarIcon,
+} as ComponentMeta<typeof StarIcon>;
+
+const Template: ComponentStory<typeof StarIcon> = (args) => <StarIcon {...args} />;
+
+export const Default = Template.bind({});
+Default.storyName = 'PriorityButton';
+Default.args = {
+  status: false,
+};
