84f0cb2cbbaeaf9a22760fa70bd176b16e00810b
Hiroshi MORISHIGE <hiroshi.morishige@gmail.com>
2021-07-11 18:51:15 +0900

    :up: update storybook setting

--- frontend/.storybook/preview.tsx
+++ frontend/.storybook/preview.tsx
@@ -11,6 +11,13 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  options: {
+    // storySort: (a, b) =>
+    //   a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+    storySort: {
+      order: ['components', ['Atoms', 'Molecules', 'Organisms', 'Templates', 'Pages']],
+    },
+  },
 };
 
 export const decorators = [
