3af76e1da9dc1b0c14955f8f7e7e2079f4315086
Hiroshi MORISHIGE <hiroshi.morishige@gmail.com>
2021-07-11 20:13:37 +0900

    :sparkles: add components index

---
+++ frontend/src/components/atoms/index.ts
@@ -0,0 +1,6 @@
+export { default as Button } from './Button';
+export { default as StarIcon } from './StarIcon';
+export { default as Input } from './Input';
+export { default as CheckBox } from './CheckBox';
+export { default as TextArea } from './TextArea';
+export { default as TrashIcon } from './TrashIcon';
