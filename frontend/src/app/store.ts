850c0f6c7652196ff2e289c3c444c07a8d3a5364
Hiroshi MORISHIGE <hiroshi.morishige@gmail.com>
2021-07-14 10:44:37 +0900

    :shower: remove counterSlice

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-import tasksReducer from '../features/tasks/tasksSlice';
+import tasksReducer from 'src/features/tasks/tasksSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     tasks: tasksReducer,
   },
 });
