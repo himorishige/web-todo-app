8875a40e2da280928d343f44c08ae0fbe74ec471
Hiroshi MORISHIGE <hiroshi.morishige@gmail.com>
2021-07-15 13:37:31 +0900

    :recycle: refactor code

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -3,26 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
-
-const localStorageMock = (() => {
-  let store: any = {};
-
-  return {
-    getItem(key: any) {
-      return store[key] || null;
-    },
-    setItem(key: any, value: any) {
-      store[key] = value.toString();
-    },
-    removeItem(key: any) {
-      delete store[key];
-    },
-    clear() {
-      store = {};
-    },
-  };
-})();
-
-Object.defineProperty(window, 'sessionStorage', {
-  value: localStorageMock,
-});
